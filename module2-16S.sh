eab09e3510d9e70426e555df5f3f62354398e471
Adam Oss <39794124+AdamOss88@users.noreply.github.com>
2024-04-02 12:29:47 +0200

    Update module2-16S.sh
    add the mod2 bash file slurm instructions

--- module2-16S.sh
+++ module2-16S.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
+#SBATCH -J module2.sh
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=180gb
+#SBATCH --time=10:00:00
+#SBATCH --constraint=cal
+#SBATCH --error=processed/module2.%J.err
+#SBATCH --output=processed/module2.%J.out
 
-{
+#load software
+module load dada2/1.26.0
+
+# add databases
+DB=~/belendm/databases/dada2/silva_nr99_v138.1_train_set.fa.gz
+speciesDB=~/belendm/databases/dada2/silva_species_assignment_v138.1.fa.gz
+
 ###make folders
 mkdir -p ./results/
 
 ###run dada2 script
-Rscript --vanilla src/dada2.R
-} 2>&1 | tee ./processed/module2.out
+Rscript --vanilla src/dada2.R $DB $speciesDB
 
 
 #echo "otu table and taxonomy table were generated !"
