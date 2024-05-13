e1ff1f868c5fd52e177433ca78bb8733b57cc177
Adam Oss <39794124+AdamOss88@users.noreply.github.com>
2024-05-13 13:20:11 +0200

    Update module1-16S.sh
    working version on slurm, walkaround with SLURM output

--- module1-16S.sh
+++ module1-16S.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 #SBATCH -J module1-16S.Raw-to-Rawr
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=10gb
-#SBATCH --time=10:00:00
+#SBATCH --mem=4gb
+#SBATCH --time=01:00:00
 #SBATCH --constraint=cal
-#SBATCH --error=reports/module1.%J.err
-#SBATCH --output=reports/module1.%J.out
-
-###load software
-module load cutadapt/4.4
-module load seqkit/2.2.0
-module load fastp/0.23.4
-
-###set locale (just in case) 
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
+#SBATCH --output=output_module1-16S.%J.out
 
 ###make folders
 mkdir -p processed/1.trimmed_primers
@@ -28,6 +18,8 @@ mkdir -p temp
 
 ###trimming 
 ##read primers
+module load seqkit/2.2.0
+
 primF=`awk 'NR==2' primers.fasta`
 primR=`awk 'NR==4' primers.fasta`
 seqkit seq -p -r -t dna primers.fasta > temp/primers_RC.fasta
@@ -36,6 +28,9 @@ primRrc=`awk 'NR==4' temp/primers_RC.fasta`
 
 ##trim primers
 cd raw_data/
+
+module load cutadapt/4.4
+
 for F in *_1.fq.gz; do
 echo "trimming " $primF " and " $primR " in " $F " and " ${F//_1.fq.gz/_2.fq.gz}
 cutadapt -g $primF -G $primR -a $primFrc -A $primRrc -n 1 \
@@ -50,6 +45,9 @@ rm temp/primers_RC.fasta
 ## quality filtering with fastp
 
 cd processed/1.trimmed_primers/
+
+module load fastp/0.23.4
+
 for F in *_1.fq.gz; do
 echo "working on " $F " and " ${F//_1.fq.gz/_2.fq.gz}
 fastp -i $F -I ${F//_1.fq.gz/_2.fq.gz} \
@@ -67,5 +65,6 @@ cd ../..
 ## run an R script to filter and trim
 #Rscript --vanilla src/filter.R
 ##
+module purge
 echo "Check the quality before going further !"
-
+mv output_module* reports/
