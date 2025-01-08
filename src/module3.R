2c365b6bf9fa80905c5cbba8bdfd638836897014
Adam Oss <39794124+AdamOss88@users.noreply.github.com>
2025-01-08 23:01:01 +0100

    Update module3.R

--- src/module3.R
+++ src/module3.R
@@ -1,19 +1,16 @@
-##needs to be adopted
-
 library(phyloseq)
-library(Biostrings)
 library(vegan)
 
 #load data
-otu_table16S = readRDS("./otu_table16S.RDS")
-tax_table16S = readRDS("./tax_table16S.RDS")
-sam_data = read.csv2("../sample_data.csv", row.names = 1)
-refseq = readDNAStringSet("refseq16S.fasta")
+otu_table = readRDS("./results/otu_table.RDS")
+tax_table = readRDS("./results/tax_table.RDS")
+#sam_data = read.csv2("../sample_data.csv", row.names = 1)
+refseq = Biostrings::readDNAStringSet("results/refseq.fasta")
 
 #phyloseq object
-bacteria = phyloseq(otu_table(otu_table16S, taxa_are_rows = F), 
-         tax_table(tax_table16S), 
-         sample_data(sam_data), 
+amplicons = phyloseq(otu_table(otu_table, taxa_are_rows = F), 
+         tax_table(tax_table), 
+#        sample_data(sam_data), 
          refseq(refseq))
 
 #Taxa You Lost
@@ -25,36 +22,36 @@ TYL = data.frame(initial = rep(NA,2),
                   rarefication = rep(NA,2),
                   final = rep(NA,2))
 row.names(TYL) = c("number of taxa","removed")
-TYL$initial = c(length(taxa_names(bacteria)),0)
+TYL$initial = c(length(taxa_names(amplicons)),0)
+
 #filter
 ##abundance remove singletons 
-table(taxa_sums(bacteria) > 2)
-bacteria_filt = prune_taxa(taxa_sums(bacteria) > 2, bacteria)
-TYL$singletons= c(length(taxa_names(bacteria_filt)),TYL$initial[1] - length(taxa_names(bacteria_filt)))
+amplicons_filt = prune_taxa(taxa_sums(amplicons) > 2, amplicons)
+TYL$singletons= c(length(taxa_names(amplicons_filt)),TYL$initial[1] - length(taxa_names(amplicons_filt)))
 ##no affiliation
-bacteria_filt = subset_taxa(bacteria_filt, Kingdom != "NA" )
-TYL$unaffiliated= c(length(taxa_names(bacteria_filt)),TYL$singletons[1] - length(taxa_names(bacteria_filt))) 
+amplicons_filt = subset_taxa(amplicons_filt, Kingdom != "NA" )
+TYL$unaffiliated= c(length(taxa_names(amplicons_filt)),TYL$singletons[1] - length(taxa_names(amplicons_filt))) 
 ##contaminants
 ###Mitochondia
-bacteria_filt = prune_taxa(
-  !taxa_names(bacteria_filt) %in% taxa_names(bacteria_filt)[apply(tax_table(bacteria_filt), 1, function(row) any(grepl("Mitochondria", row)))],
-bacteria_filt)
-TYL$mitochondria= c(length(taxa_names(bacteria_filt)),TYL$unaffiliated[1]-length(taxa_names(bacteria_filt)))
+amplicons_filt = prune_taxa(
+  !taxa_names(amplicons_filt) %in% taxa_names(amplicons_filt)[apply(tax_table(amplicons_filt), 1, function(row) any(grepl("Mitochondria", row)))],
+  amplicons_filt)
+TYL$mitochondria= c(length(taxa_names(amplicons_filt)),TYL$unaffiliated[1]-length(taxa_names(amplicons_filt)))
 ###Chloroplasts
-bacteria_filt = prune_taxa(
-  !taxa_names(bacteria_filt) %in% taxa_names(bacteria_filt)[apply(tax_table(bacteria_filt), 1, function(row) any(grepl("Chloroplast", row)))],
-  bacteria_filt)
-TYL$chloroplasts= c(length(taxa_names(bacteria_filt)),TYL$mitochondria[1] -length(taxa_names(bacteria_filt)))
+amplicons_filt = prune_taxa(
+  !taxa_names(amplicons_filt) %in% taxa_names(amplicons_filt)[apply(tax_table(amplicons_filt), 1, function(row) any(grepl("Chloroplast", row)))],
+  amplicons_filt)
+TYL$chloroplasts= c(length(taxa_names(amplicons_filt)),TYL$mitochondria[1] -length(taxa_names(amplicons_filt)))
 #rarefy
 set.seed(456)
 
-pdf("rarecurve_16S.pdf")
-rare_curve = vegan::rarecurve(as.matrix(as.data.frame(phyloseq::otu_table(bacteria_filt))), step = 100 , cex=0.5)
+pdf("reports/rarecurve.pdf")
+rare_curve = vegan::rarecurve(as.matrix(as.data.frame(phyloseq::otu_table(amplicons_filt))), step = 100 , cex=0.5)
 dev.off()
 
-bacteria_rare = phyloseq::rarefy_even_depth(bacteria_filt, rngseed=456, sample.size=min(sample_sums(bacteria_filt)), replace=F)
-TYL$rarefication= c(length(taxa_names(bacteria_rare)),TYL$chloroplasts[1] - length(taxa_names(bacteria_rare)))
-TYL$final= c(length(taxa_names(bacteria_rare)),0)
-write.csv(TYL, file = "results/filtering_bacteria.csv")
+amplicons_rare = phyloseq::rarefy_even_depth(amplicons_filt, rngseed=456, sample.size=min(sample_sums(amplicons_filt)), replace=F)
+TYL$rarefication= c(length(taxa_names(amplicons_rare)),TYL$chloroplasts[1] - length(taxa_names(amplicons_rare)))
+TYL$final= c(length(taxa_names(amplicons_rare)),0)
+write.csv(TYL, file = "results/filtering_amplicons.csv")
 
-saveRDS(bacteria_rare, file= "objects/Plastic_16S_filter_rare.rds")
+saveRDS(amplicons_rare, file= "./results/amplicons_filter_rare.rds")
